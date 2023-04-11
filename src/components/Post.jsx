d7440be42f23c22cf1a7271e2901f5f4162b8c01
Hung Luu <hungluu111103@gmail.com>
2023-04-11 18:19:21 +0700

    fix cloudinary

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,19 +4,9 @@ import axios from 'axios';
 
 const Post = (props) => {
 
-  const [imageUrl, setImageUrl] = useState("");
-
-  useEffect(() => {
-    const fetchImage = async () => {
-      const response = await axios.get(process.env.REACT_APP_PIKTURE_API_URL + `api/Post/${props.post.id}`, { responseType: 'blob' });
-      setImageUrl(URL.createObjectURL(response.data));
-    }
-    fetchImage();
-  }, [])
-  
   return (
     <div id="Post">
-      <img className="postImg" src={imageUrl} alt="" />
+      <img className="postImg" src={props.post.imageUrl} alt="" />
       <img className="postAvatarImg" src={props.post.userAvatarUrl} alt="" width="30px" height="30px"/>
       <div className="postUserName">{props.post.userName}</div>
     </div>
